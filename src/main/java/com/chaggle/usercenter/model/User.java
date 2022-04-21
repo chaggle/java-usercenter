23f2ac44f32a783dbffd1a88b00ea4b3b5288d86
chaggle <chaggle@foxmail.com>
2022-04-21 15:22:58 +0800

    first branch and test

---
+++ src/main/java/com/chaggle/usercenter/model/User.java
@@ -0,0 +1,11 @@
+package com.chaggle.usercenter.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
